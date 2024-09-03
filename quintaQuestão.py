2ed4fd83ca46b6de9f8b98280c1b71fceee09e44
focarica <a.vinicius0042@gmail.com>
2024-09-03 17:53:22 -0300

    resolução quinta questão

---
+++ quintaQuestão.py
@@ -0,0 +1,14 @@
+def invertString(string: str) -> str:
+    p1, p2 = string[0], string[-1]
+    
+    string = list(string)
+    
+    for i in range(len(string)//2):
+        string[i], string[-1-i] = p2, p1
+        
+        p1, p2 = string[i+1], string[-1-(i+1)]
+        
+    return ''.join(string)
+    
+
+print(invertString("hoje"))
